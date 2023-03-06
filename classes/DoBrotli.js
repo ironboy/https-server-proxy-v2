a0962df58cd1b47cc9fb1c26532524c63ed5e61a
Ironboy <ironboy@users.noreply.github.com>
2023-03-06 07:18:11 +0100

    working on brotli

--- classes/DoBrotli.js
+++ classes/DoBrotli.js
@@ -1,9 +1,48 @@
+const fs = require('fs');
+const path = require('path');
 const crypto = require('crypto');
+const AsyncBrotli = require('./AsyncBrotli');
 
 module.exports = class DoBrotli {
 
-  constructor(stream, response, responseHeaders, status) {
+  static shallCompress = ct =>
+    !(ct.includes('event-stream')) && (
+      ct.includes('text') ||
+      ct.includes('javascript') ||
+      ct.includes('json') ||
+      ct.includes('svg')
+    );
+
+  static brotliFastCompress = new AsyncBrotli({ quality: 1 });
+  static brotliRecompress = new AsyncBrotli({ quality: 11 });
+
+  constructor(stream, response, reqH, resH, status) {
+    Object.assign(this, { stream, response, reqH, resH, status });
+    this.willBrotli = response && response.body &&
+      !resH['content-encoding'] &&
+      reqH['accept-encoding'].includes('br') &&
+      DoBrotli.shallCompress(resH['content-type']);
+    console.log("willBrotli", this.willBrotli);
     this.willBrotli = false;
+    this.willBrotli && this.respond();
   }
+
+  async respond() {
+    const reader = response.body.getReader();
+    let all = [];
+    while (reader) {
+      const { value, done } = await reader.read();
+      value && all.push(value);
+      if (done) { break; }
+    }
+    all = Buffer.concat(all);
+    this.responseHeaders['content-length'] = all.length;
+    this.responseHeaders['encoding']
+    this.stream.write(await this.brotliFastCompress(all));
+    this.stream.end();
+  }
+
+
+
 
 }
--- classes/ProxyServer.js
+++ classes/ProxyServer.js
@@ -5,34 +5,58 @@ module.exports = class ProxyServer {
 
   async web(stream, headers, target) {
     let url = headers[':path'];
-    const requestHeaders = this.getHeaders({ headers });
     const requestBody = await this.getRequestBody(stream);
-    requestHeaders['content-length'] = (requestBody || '').length + '';
-    requestHeaders['content-length'] === '0' && delete requestHeaders['content-length'];
+    const requestHeaders = this.getHeaders({ headers }, requestBody);
     if (await this.handle304(stream, headers, requestHeaders, target)) {
       return;
     }
-    const response = await fetch(target + url, {
-      method: headers[':method'],
+    const method = headers[':method'], response = await fetch(target + url, {
+      method,
       headers: requestHeaders,
       body: requestBody
     });
     const responseHeaders = this.getHeaders(response);
-    responseHeaders['x-powered-by'] = 'Love';
-    !responseHeaders['content-security-policy']
-      && (responseHeaders['content-security-policy'] = 'upgrade-insecure-requests;');
-    responseHeaders['access-control-allow-origin'] = '*';
-    this.makeResponse(stream, response, responseHeaders);
+    this.makeResponse(stream, response, requestHeaders, responseHeaders, method);
   }
 
-  getHeaders(obj) {
-    return Object.fromEntries(
+  async handle304(stream, headers, reqH, target) {
+    if (headers[':method'] !== 'GET') { return; }
+    const response = await fetch(target + headers[':path'], {
+      method: 'HEAD', headers: reqH
+    });
+    const resH = this.getHeaders(response);
+    let okTo304 = null;
+    reqH['if-none-match']
+      && (okTo304 = reqH['if-none-match'] === resH['etag']);
+    (okTo304 || okTo304 === null) && reqH['if-modified-since']
+      && (okTo304 = reqH['if-modified-since'] === resH['last-modified']);
+    if (okTo304) {
+      delete resH['content-length'];
+      delete resH['content-encoding'];
+      this.makeResponse(stream, null, reqH, resH, 'GET', 304);
+      return true
+    }
+  }
+
+  getHeaders(obj, requestBody) {
+    const h = Object.fromEntries(
       (obj.headers.entries ?
         [...obj.headers.entries()] : Object.entries(obj.headers))
         .map(e => [e[0].toLowerCase(), e[1]])
         .filter(e => e[0][0] !== ':')
-        .filter(e => !['connection', 'keep-alive'].includes(e[0]))
-    );
+        .filter(e => !['connection', 'keep-alive'].includes(e[0])));
+    if (!(obj instanceof Response)) {
+      h['content-length'] = (requestBody || '').length + '';
+      h['content-length'] === '0' && delete h['content-length'];
+    }
+    else {
+      Object.assign(h, {
+        'x-powered-by': 'Love',
+        'content-security-policy': 'upgrade-insecure-requests;',
+        'access-control-allow-origin': '*'
+      });
+    }
+    return h;
   }
 
   // call async
@@ -44,39 +68,13 @@ module.exports = class ProxyServer {
     });
   }
 
-  async handle304(stream, headers, reqH, target) {
-    if (headers[':method'] !== 'GET') { return; }
-    const response = await fetch(target + headers[':path'], {
-      method: 'HEAD',
-      headers: reqH
-    });
-    const resH = this.getHeaders(response);
-    let okTo304 = null;
-    reqH['if-none-match']
-      && (okTo304 = reqH['if-none-match'] === resH['etag']);
-    (okTo304 || okTo304 === null) && reqH['if-modified-since']
-      && (okTo304 = reqH['if-modified-since'] === resH['last-modified']);
-    okTo304 && this.makeResponse(stream, null, resH, 304);
-    return okTo304;
-  }
-
-  async makeResponse(stream, response, responseHeaders, status = response.status) {
+  async makeResponse(stream, response, reqH, resH, method, status = response.status) {
     if (stream.closed) { return; }
-    if (new DoBrotli(stream, response, responseHeaders, status).willBrotli) { return; }
-    stream.respond({
-      ':status': status,
-      ...responseHeaders
-    });
-    /*const reader = response?.body?.getReader();
-    while (reader) {
-      // console.log("IN LOOP")
-      const { value, done } = await reader.read();
-      //  console.log((value && value.length) || 'no value', 'done', done)
-      value && stream.write(value);
-      if (done) { break; }
+    if (method === 'GET' && +status === 200 &&
+      (new DoBrotli(stream, response, reqH, resH, status)).willBrotli) {
+      return;
     }
-    //console.log("OUT OF LOOP")
-    stream.end();*/
+    stream.respond({ ':status': status, ...resH });
     const bodyStream = response?.body;
     if (!bodyStream) { stream.end(); }
     else {
@@ -84,7 +82,6 @@ module.exports = class ProxyServer {
       // (for example when 'scrubbing' through an mp4) - catch and ignore
       await pipeline(bodyStream, stream).catch(e => false);
     }
-
   }
 
 }
