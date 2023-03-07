285dd0c385af401fa0d132619b6d70711c624d53
Ironboy <ironboy@users.noreply.github.com>
2023-03-07 14:22:57 +0100

    import cache

--- utils/brotliPrunerWorker.js
+++ utils/brotliPrunerWorker.js
@@ -6,7 +6,7 @@ parentPort.on('message', async ({ id, maxSizeBytes, dir }) => {
   const startTime = Date.now();
   let files = (await fs.readdir(dir))
     .filter(x => x.slice(-3) === '.br');
-  let stats = await Promise.all(files.map(x => fs.stat(path.join(dir, x))));
+  let stats = await Promise.all(files.map(x => fs.stat(path.join(dir, x)).catch(e => 0)));
   let joined = files.map((x, i) => [x, stats[i].mtime,
     Math.ceil(stats[i].size / stats[i].blksize) * stats[i].blksize]);
   joined.sort((a, b) => a[1] > b[1] ? -1 : 1)
