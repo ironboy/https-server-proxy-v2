6c89742652272c00cb5418459479f73fab6bb0f8
Ironboy <ironboy@users.noreply.github.com>
2023-03-07 09:30:46 +0100

    cleanup

--- utils/touch.js
+++ utils/touch.js
@@ -3,7 +3,6 @@ const fs = require('fs/promises');
 // Touch a file
 module.exports = async function touch(filePath) {
   const time = new Date();
-
   await fs.utimes(filePath, time, time).catch(async function (err) {
     if ('ENOENT' !== err.code) {
       throw err;
